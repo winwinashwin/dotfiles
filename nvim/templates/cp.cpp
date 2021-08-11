7a3ff143ff38f8819fbfd5533066a215b4136828
Ashwin A Nayar <ashwin5059198@gmail.com>
2021-08-11 14:47:38 +0530

    add files for cp

---
+++ nvim/templates/cp.cpp
@@ -0,0 +1,24 @@
+/**
+ * @author aan2001
+ * @date %date%
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+template<typename T> inline T inf() { return numeric_limits<T>::infinity(); }
+
+static constexpr int dirx[8] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+static constexpr int diry[8] = { 0, 1, -1, 0, -1, 1, -1, 1 };
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+#ifdef LOCAL
+    freopen("stdin.txt", "r", stdin);
+#endif
+
+    /*= SOLUTION =*/
+}
